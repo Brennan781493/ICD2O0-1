aead8f7bc411de29ded5aa7f205903efb3f0d8c4
Brennan781493 <781493@pdsb.net>
2024-09-24 14:23:21 -0400

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -1,1 +1,27 @@
+package assignment.brennan;
 
+/**
+ *
+ * @author 781493
+ */
+public class AssignmentBrennan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     
+        importjava.lang.math;
+        importjava.util.Scanner;
+
+        class myClass { // you always need this, call it whatever class you want.
+
+            // however, the name of the class should be the same as the 
+            // filename (in this case, myClass.java)
+            public static void main(String[] args) {
+
+                // ALL of your remaining code goes here
+    }
+    
+}
