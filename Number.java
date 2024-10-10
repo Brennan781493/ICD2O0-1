063d5df6e3740b6c85bf171546503a681c9f9b76
Brennan781493 <781493@pdsb.net>
2024-10-10 08:20:55 -0400

    Update Number.java

--- Number.java
+++ Number.java
@@ -1,6 +1,6 @@
 class number {
   public static void main(String[] args) {
         int StepsInADay = 10,000;
-        System.out.println("The number of keys on a keyboard is " + numberOfKeys);
+        System.out.println("The number steps I take in a day " + StepsInADay);
     }
 }
