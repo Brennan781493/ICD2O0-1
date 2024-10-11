c68ec7e90d43de3d87853f1d09ae08af7bc09f43
Brennan781493 <781493@pdsb.net>
2024-10-10 21:56:39 -0400

    Update lesson1.java

--- lesson1.java
+++ lesson1.java
@@ -1,6 +1,6 @@
 class Testing {
   public static void main(String [] args) {
     String name = "Brennan";
-      System.out.println("Hello" + name);
+      System.out.println("Hello, " + name);
   }
 }
