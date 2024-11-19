a586aaf982c89bf840bffc321c3c355621447fbf
Brennan781493 <781493@pdsb.net>
2024-11-19 12:38:12 -0500

    Update and rename ASimpleConsoleCalculator.java to Unit6L2.java

--- Unit6L2.java
+++ Unit6L2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ASimpleConsoleCalculator {
+public class Unit6L2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
