dfc9f78e11a05cbe240827d3e49272d9419898b2
Brennan781493 <781493@pdsb.net>
2024-11-19 12:33:01 -0500

    Update and rename A Simple Console Calculator.java to ASimpleConsoleCalculator.java

--- ASimpleConsoleCalculator.java
+++ ASimpleConsoleCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Calc {
+public class ASimpleConsoleCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
