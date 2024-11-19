48708d4f4d354610e37b183bf7e81f5f4202fede
Brennan781493 <781493@pdsb.net>
2024-11-19 12:41:50 -0500

    Update and rename MakingAMenu.java to Unit6L6.java

--- Unit6L6.java
+++ Unit6L6.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
 
-class MakingAMenu {
+class Unit6L6 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         String choice = "";
